9186baaa8f145610c6664d78c0d63c0f09355a1f
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-04-17 15:22:17 +0200

    Update file_download.js

--- file_download.js
+++ file_download.js
@@ -4,7 +4,7 @@
  * @param {string} filename Name of the file to download
  * @returns {void}
  */
-function fileDownload(data = new Blob(), filename = '') {
+export function fileDownload(data = new Blob(), filename = '') {
     const link = document.createElement('a');
     link.href = window.URL.createObjectURL(data);
     link.download = filename;

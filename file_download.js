6363cf615499c27e34d6e96b50ef93daa8f0c339
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-04-17 15:09:57 +0200

    Create file_download.js

---
+++ file_download.js
@@ -0,0 +1,12 @@
+/**
+ * This function provides a way to download blob files from the browser.
+ * @param {Blob} data File blob object
+ * @param {string} filename Name of the file to download
+ * @returns {void}
+ */
+function fileDownload(data = new Blob(), filename = '') {
+    const link = document.createElement('a');
+    link.href = window.URL.createObjectURL(data);
+    link.download = filename;
+    link.click()
+}

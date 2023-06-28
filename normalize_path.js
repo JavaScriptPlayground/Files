e957188eb296aec423df6985a5cf9d8df7f82bf2
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-28 14:18:06 +0200

    Update normalize_path.js

--- normalize_path.js
+++ normalize_path.js
@@ -1,6 +1,16 @@
-export function normalizePath(pathArray = '', separator = '/') {
+/**
+ * This function takes a path an a separator and returns the normalized path.
+ * ```
+ * normalizePath('this///////is/a//very/messed//./up//.//long/../path//to///a//..../special/../file///.///');
+ * // /this/is/a/very/messed/up/long/../path/to/a/../../special/../file/
+ * ```
+ * @param {string} path The path to normalize
+ * @param {string} separator The separator for the path to use 
+ * @returns {string} The normalized path
+ */
+export function normalizePath(path = '', separator = '/') {
     const regExMatchSeparatorPosionReplacement = /\/{2,}(?:\.\/+)?|\/\.\/|(?<=\/(?:\.\.)+)\.\/|(?<=\/(?:\.\.)+)(?!\/)/g;
-    return separator + pathArray.replace(
+    return separator + path.replace(
         regExMatchSeparatorPosionReplacement,
         separator
     ).replace(

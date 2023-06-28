ce85fc77979ed41166dcd538d132f70908fd84ad
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-28 12:30:59 +0200

    Update path_default_file.js

--- path_default_file.js
+++ path_default_file.js
@@ -1,6 +1,10 @@
 /**
  * This function takes a path and returns the path fith the default file.
  * If the path is already a path to a file than this path is returned.
+ * ```
+ * pathDefaultFile('path/to/file.txt', 'default_file.txt') // 'path/to/file.txt'
+ * pathDefaultFile('./path/to', 'default_file.txt') // './path/to/default_file.txt'
+ * ```
  * @param {string} path The path to add the default file to
  * @param {string} defaultFile The default file
  * @returns {string} The new path with the default file

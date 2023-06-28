d8f7b7c74dd76a2c62c71dc353b9e9aa2a5244af
Mqx <62719703+Mqxx@users.noreply.github.com>
2023-06-28 14:21:03 +0200

    Update normalize_path.js

--- normalize_path.js
+++ normalize_path.js
@@ -2,19 +2,19 @@
  * This function takes a path an a separator and returns the normalized path.
  * ```
  * normalizePath('this///////is/a//very/messed//./up//.//long/../path//to///a//..../special/../file///.///');
- * // /this/is/a/very/messed/up/long/../path/to/a/../../special/../file/
+ * // this/is/a/very/messed/up/long/../path/to/a/../../special/../file
  * ```
  * @param {string} path The path to normalize
  * @param {string} separator The separator for the path to use 
  * @returns {string} The normalized path
  */
 export function normalizePath(path = '', separator = '/') {
-    const regExMatchSeparatorPosionReplacement = /\/{2,}(?:\.\/+)?|\/\.\/|(?<=\/(?:\.\.)+)\.\/|(?<=\/(?:\.\.)+)(?!\/)/g;
-    return separator + path.replace(
+    const regExMatchSeparatorPosionReplacement = /\/{2,}(?:\.\/+)?|(?<=\/(?:\.\.)+)\.\/|(?<=\/(?:\.\.)+)(?!\/)/g;
+    return path.replace(
         regExMatchSeparatorPosionReplacement,
         separator
     ).replace(
         /^\/+|\/+$/,
         ''
-    ) + separator;
+    );
 }
